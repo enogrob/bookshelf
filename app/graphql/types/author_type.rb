610a7cbbe091554dedb9e55f43a333e5696ac3e1
Roberto Nogueira <enogrob@gmail.com>
2023-05-25 10:57:16 -0300

    performed-2.15

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -7,4 +7,8 @@ class Types::AuthorType < Types::BaseObject
   field :yob, Int, null: false
   field :is_alive, Boolean, null: true
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :full_name, String, null: true
+  def full_name
+    "#{object.first_name} #{object.last_name}"
+  end
 end
