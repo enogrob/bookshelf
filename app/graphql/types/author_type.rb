658ab45751099df9b37b092e16283a1a56daead1
Roberto Nogueira <enogrob@gmail.com>
2023-05-25 10:47:18 -0300

    performed-2.14

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -2,7 +2,7 @@ class Types::AuthorType < Types::BaseObject
   description "One author"
 
   field :id, ID, null: false
-  field :first_name, String, null: true
+  field :first_name, String, null: false
   field :last_name, String, null: true
   field :yob, Int, null: false
   field :is_alive, Boolean, null: true
