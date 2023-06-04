00e432944f9a8dde2de6d3418450fa3f020fdd2e
Roberto Nogueira <enogrob@gmail.com>
2023-06-04 03:28:32 -0300

    performed-upto-3.21

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -15,3 +15,13 @@ class Types::AuthorType < Types::BaseObject
   field :coordinates, Types::CoordinatesType, null: false
   field :publication_years, [Int], null: false
 end
+
+class Types::AuthorInputType < GraphQL::Schema::InputObject
+  graphql_name "AuthorInputType"
+  description "All the attributes needed to create an author"
+
+  argument :first_name, String, required: false
+  argument :last_name, String, required: false
+  argument :yob, Int, required: false
+  argument :is_alive, Boolean, required: false
+end
