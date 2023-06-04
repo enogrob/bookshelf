26e2f5d55d3eb08139f5e0a9931767881e7a202c
Roberto Nogueira <enogrob@gmail.com>
2023-06-04 04:26:41 -0300

    performed-upto-3.24

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,15 +1,21 @@
 module Types
   class MutationType < Types::BaseObject
-    # field :create_author, Types::AuthorType, null: true, description: "Create an author" do
-    #   argument :first_name, String, required: false
-    #   argument :last_name, String, required: false
-    #   argument :yob, Int, required: false
-    #   argument :is_alive, Boolean, required: false
-    # end
-    # def create_author(first_name:, last_name:, yob:, is_alive:)
-    #   Author.create(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
-    # end
-
     field :create_author, Types::AuthorType, mutation: Mutations::CreateAuthor
+    field :update_author, Boolean, null: false, description: "Update an author" do
+      argument :author, Types::AuthorInputType, required: true
+    end
+    field :delete_author, Boolean, null: false, description: "Delete an author" do
+      argument :id, ID, required: true
+    end
+
+    def update_author(author:)
+      existing = Author.where(id: author[:id]).first
+      existing.update author.to_h
+    end
+
+    def delete_author(id:)
+      Author.where(id: id).destroy_all
+      true
+    end
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,4 @@
 class Author < ApplicationRecord
-  # def full_name
-  #   "#{first_name} #{last_name}"
-  # end
   def coordinates
     [rand(90), rand(90)]
   end
@@ -9,4 +6,6 @@ class Author < ApplicationRecord
   def publication_years
     (1..rand(10)).to_a.map{1900 - rand(100)}
   end
+
+  validates :last_name, presence: true
 end
